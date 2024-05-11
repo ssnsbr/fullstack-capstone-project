a9cd682bb74ed374e92c504fcb0bcfee105e8028
sasan <sasan.saberi.m@gmail.com>
2024-05-11 12:26:10 -0400

    w3 Hands-on Lab: Create the Landing Page

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,15 +1,33 @@
+// app.js
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
+const pinoLogger = require('./logger')
 
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
-
 
 const app = express();
-app.use("*", cors());
-const port = 3060;
+const port = 3050;
+
+// Serve static files from the public directory (for home.html)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files for React App from a subdirectory
+app.use('/app', express.static(path.join(__dirname, 'public', 'react-app')));
+
+
+// Route for Home Page - Serve home.html as the default page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+// Serve the React app's index.html for any other requests under /app
+app.get('/app/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
+});
+
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
@@ -21,36 +39,29 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes  = require('./routes/searchRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
-
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
 
+// Default Route for React App
+app.get('/app/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
+});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.get("/", (req, res) => {
-    res.send("Inside the server")
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
