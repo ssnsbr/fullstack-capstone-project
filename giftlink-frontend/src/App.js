6f7af68cc0e1683d5dfaca1b10e42b728f944105
ssnsbr <32017003+ssnsbr@users.noreply.github.com>
2024-05-12 17:25:28 +0330

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import LoginPage from './components/LoginPage/LoginPage';
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar/Navbar';
 import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
     return (
         <>
             <Navbar />
